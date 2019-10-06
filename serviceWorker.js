ac2dc4a7b4f4898c6aefa06506e9343552f2895e
Antonster <Antontitov1993@gmail.com>
2019-10-06 23:48:06 +0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,63 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "3389a8fd92c7f58c2b3780fecc462dbb"
+  },
+  {
+    "url": "logo192.ec1737a3.png",
+    "revision": "33dbdd0177549353eeeb785d02c294af"
+  },
+  {
+    "url": "logo512.0169c94f.png",
+    "revision": "917515db74ea8d1aee6a246cfbcc0b45"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "bc1b615fc60e82958f41725bd7d26994"
+  },
+  {
+    "url": "reset.7c5a02e2.css",
+    "revision": "637250798755c14ec47a9f840b544b1a"
+  },
+  {
+    "url": "src.66861ced.js",
+    "revision": "d62b62e1b2636285bef081bf21ca785d"
+  },
+  {
+    "url": "src.c317f8dc.css",
+    "revision": "46b7cb4f8fc32ff2c86642b3818e9872"
+  },
+  {
+    "url": "styles.1d979f12.css",
+    "revision": "925b3b486d748bb6d6465efd2928d91a"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
